dd2f9e2b8bc3d54ba0c53e240b58e1da4d921cb3
Luijo <luisjosemartin4@gmail.com>
2020-06-20 16:00:41 -0300

    Agrega tests esSenial y seEnojo. Cambio en funcion subseqEnojada

--- reunionesRemotas/src/auxiliares.cpp
+++ reunionesRemotas/src/auxiliares.cpp
@@ -14,9 +14,11 @@ bool enRango(senial s, int prof)
     int umbralSuperior = pow(2, prof - 1) - 1;
     int umbralInferior = -pow(2, prof - 1);
     bool res = true;
+
     for (int i = 0; i < s.size() && res; i++) {
-        res = res && (umbralInferior <= s[i]) && (s[i] <= umbralSuperior);
+        res = ( umbralInferior <= s[i]  &&  s[i] <= umbralSuperior );
     }
+
     return res;
 }
 
@@ -26,25 +28,25 @@ bool profValida(int prof)
 }
 
 bool duraMasDe(senial s, int freq, float seg)
-{ //preguntar
+{
     return s.size() >= freq * seg;
 }
 
 bool esValida(senial s, int prof, int freq)
 {
-    return freqValida(freq) && enRango(s, prof) && profValida(prof) && duraMasDe(s, freq, 1);
+    return freqValida(freq)  &&  enRango(s, prof)  &&  profValida(prof)  &&  duraMasDe(s, freq, 1);
 }
 
 
 /************* seEnojo **************/
 float tono(senial s)
 {
-
     float sum = 0;
 
     for (int i = 0; i < s.size(); i++) {
-        sum = sum + abs(s[i]);  //hace falta pasarlo a double??
+        sum += abs(s[i]);
     }
+
     return sum / s.size();
 }
 
@@ -55,60 +57,77 @@ bool superaUmbral(senial s, int umbral)
 
 bool existeSubseqEnojada(senial s, int umbral, int prof, int freq)
 {
-    for (int i = 0; i < s.size() - 2 * freq; ++i) {
-        for (int j = i + 2 * freq; j < s.size(); ++j) {
+    int cantMinimaDeElementos = 2 * freq;
+
+    if ( s.size() < cantMinimaDeElementos ) return false;
+
+    bool res = superaUmbral(s, umbral);
+
+    for (int i = 0; ( i <= s.size() - cantMinimaDeElementos ) && !res; ++i) {
+        for (int j = i + cantMinimaDeElementos; ( j <= s.size() ) && !res; ++j) {
             senial subseq = senial(s.begin() + i, s.begin() + j);
-            if (superaUmbral(subseq, umbral))
-                return true;
+
+            res = superaUmbral(subseq, umbral);
         }
     }
-    return false;
+
+    return res;
 }
 
 
 /************ esReunionValida ******/
 bool esMatriz(vector<pair<senial, hablante>> r)
 {
-    for (int i = 0; i < r.size(); ++i) {
-//        if ( typeid(r[i]) != typeid(pair<senial, hablante>) ) return false;
-    } //comparo los tipos (que sean de tipo pair<senial, hablante>
-    return true;
-//esta bien que sea asi la entrada? me parece que entonces no tendria sentido ver si es matriz
-//preguntar si mas bien no seria comprobar la longitud de las señales
+    bool res = true;
+    senial senialModelo = r[0].first;
+
+    for (int i = 0; i < r.size() && res; ++i) {
+        senial senial_i = r[i].first;
+
+        res = ( senialModelo.size() == senial_i.size() );
+    }
+
+    return res;
 }
 
 bool senialesValidas(reunion r, int prof, int freq)
 {
-    for (int i = 0; i < r.size(); ++i) {
-        if (!esValida(r[i].first, prof, freq))
-            return false;
+    bool res = true;
+
+    for (int i = 0; i < r.size() && res; ++i) {
+        res = esValida(r[i].first, prof, freq);
     }
-    return true;
+
+    return res;
 }
 
 bool todosHablantesDistintos(reunion r)
 {
-    for (int i = 0; i < r.size(); ++i) {
-        for (int j = i + 1; j < r.size(); ++j) {
-            if (r[i].second == r[j].second)
-                return false;
+    bool repetidos = false;
+
+    for (int i = 0; i < r.size() && !repetidos; ++i) {
+        for (int j = i + 1; j < r.size() && !repetidos; ++j) {
+            repetidos = ( r[i].second == r[j].second );
         }
     }
+
     return true;
 }
 
 bool hablantesDeReunionValidos(reunion r, int prof, int freq)
-{ //para que recibe aca prof y freq??
+{
     bool res = todosHablantesDistintos(r);
+
     for (int i = 0; i < r.size() && res; ++i) {
-        res = res && (0 <= r[i].second) && (r[i].second < r.size());
+        res = ( 0 <= r[i].second  &&  r[i].second < r.size() );
     }
+
     return res;
 }
 
 bool reunionValida(reunion r, int prof, int freq)
 {
-    return (r.size() > 0) && esMatriz(r) && senialesValidas(r, prof, freq) && hablantesDeReunionValidos(r, prof, freq);
+    return r.size() > 0  &&  esMatriz(r)  &&  senialesValidas(r, prof, freq)  &&  hablantesDeReunionValidos(r, prof, freq);
 }
 
 
@@ -118,7 +137,6 @@ void acelerarSenial(senial &s, int prof, int freq)
     for (int i = 0; i < s.size(); ++i) {
         s.erase(s.begin() + i);
     }
-    return;
 }
 
 
@@ -127,6 +145,7 @@ void ralentizarSenial(senial &s, int prof, int freq)
 {
     int longFinal = 2 * s.size() - 1;
     senial salida(longFinal, 0);
+
     for (int i = 0; i < salida.size(); ++i) {
         int valor;
         if (i % 2 == 0) {
@@ -135,8 +154,8 @@ void ralentizarSenial(senial &s, int prof, int freq)
             salida[i] = (s[i / 2] + s[i / 2 + 1]) / 2;
         }
     }
+
     s = salida;
-    return;
 }
 
 
@@ -145,6 +164,7 @@ vector<hablante> hablantesConTonosElevados(reunion r, int freq, int prof)
 {
     vector<hablante> hablantes = { };
     float mayorTono = 0;
+
     for (int i = 0; i < r.size(); ++i) {
         float iesimoTono = tono(r[i].first);
 
@@ -156,6 +176,7 @@ vector<hablante> hablantesConTonosElevados(reunion r, int freq, int prof)
             hablantes.push_back(r[i].second);
         }
     }
+
     return hablantes;
 }
 
@@ -208,7 +229,7 @@ bool superaUmbral(int valor, int umbral)
 
 void actualizarIndicesYFlags(int &inicioSilencio, int indiceActual, bool &outCandidatoSilencio, bool &outEsSilencio, int freq)
 {
-    if (outCandidatoSilencio == false) {
+    if ( !outCandidatoSilencio ) {
         inicioSilencio = indiceActual;
     }
     outCandidatoSilencio = true;
@@ -238,7 +259,7 @@ vector<intervalo> obtenerSilencios(senial s, int freq, int umbral)
 
     for (int i = 0; i < s.size(); i++) {
 
-        if (superaUmbral(s[i], umbral) == false) {
+        if ( !superaUmbral(s[i], umbral) ) {
             actualizarIndicesYFlags(inicioSilencio, i, candidatoASilencio, esSilencio, freq);
         }
 
@@ -304,7 +325,7 @@ void escribirSenial(senial s, string nombreArchivo)
 
     strncpy(buf, nombreArchivo.c_str(), 100);
     outputArchivo.open(buf, ios_base::app);
-    if (outputArchivo.is_open() == false) {
+    if ( !outputArchivo.is_open() ) {
         return;
     }
 
--- reunionesRemotas/tests/seEnojo.cpp
+++ reunionesRemotas/tests/seEnojo.cpp
@@ -14,4 +14,85 @@ TEST(seEnojoTEST, senialMenorADosSegundos){
     ASSERT_FALSE(seEnojo(s, umbral, prof, freq));
 }
 
+TEST(seEnojoTEST, senialMenorADosSegundosTonoSuperaElUmbral){
+    senial s = {11,33,-32,41,66,0,-21,-118,99,-15,7};
+    int prof = 8;
+    int freq = 10;
+    int umbral = 3;
+
+    ASSERT_FALSE(seEnojo(s, umbral, prof, freq));
+}
+
+TEST(seEnojoTEST, senialMenorADosSegundosTodosCeros){
+    senial s(15,0);
+    int prof = 8;
+    int freq = 10;
+    int umbral = 3;
+
+    ASSERT_FALSE(seEnojo(s, umbral, prof, freq));
+}
+
+TEST(seEnojoTEST, senialMayorADosSegundosTodosCeros){
+    senial s(25,0);
+    int prof = 8;
+    int freq = 10;
+    int umbral = 3;
+
+    ASSERT_FALSE(seEnojo(s, umbral, prof, freq));
+}
+
+TEST(seEnojoTEST, senialEnojadaCompleta){
+    senial s = {5,5,6,-8,123,14,11,-29,-29,-29,-8,9,-15,7,-12,65,24,34,24,5,5,6,-8,123,14,11,-29,-29,-29,-8,9,-15};
+    int prof = 8;
+    int freq = 10;
+    int umbral = 3;
+
+    ASSERT_TRUE(seEnojo(s, umbral, prof, freq));
+}
+
+TEST(seEnojoTEST, senialConTonoIgualAlUmbral){
+    senial s(20,-3);
+    int prof = 8;
+    int freq = 10;
+    int umbral = 3;
+
+    ASSERT_FALSE(seEnojo(s, umbral, prof, freq));
+}
+
+TEST(seEnojoTEST, senialEnojadaConLongitudMinima){
+    senial s(20,-5) ;
+    int prof = 8;
+    int freq = 10;
+    int umbral = 3;
+
+    ASSERT_TRUE(seEnojo(s, umbral, prof, freq));
+}
+
+TEST(seEnojoTEST, senialEnojadaConSubseqMinimaEnojada){
+    senial s = {0,1,1,0,0,0,0,1,0,2,4,-2,-4,3,3,-3,3,0,6,5,1,-1,-5,3,3,-3,-3,5,-2,0,0,0,0,0,0,1,-2,2,1,0,0,3};
+    int prof = 8;
+    int freq = 10;
+    int umbral = 3;
+
+    ASSERT_TRUE(seEnojo(s, umbral, prof, freq));
+}
+
+TEST(seEnojoTEST, senialNoEnojadaConTodasMuestrasMenoresAlUmbral){
+    senial s(100,-2);
+    int prof = 8;
+    int freq = 10;
+    int umbral = 3;
+
+    ASSERT_FALSE(seEnojo(s, umbral, prof, freq));
+}
+
+TEST(seEnojoTEST, senialEnojadaConMayoriaDeMuestrasIgualesACero){
+    senial s = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,120,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+    int prof = 8;
+    int freq = 10;
+    int umbral = 3;
+
+    ASSERT_TRUE(seEnojo(s, umbral, prof, freq));
+}
+
 
