880ddf23a068b0e6c2170ca8cb384e370454412b
Luijo <luisjosemartin4@gmail.com>
2020-06-28 18:16:56 -0300

    Incluye analisis de cobertura

---
+++ reunionesRemotas/cmake-build-debug/CMakeFiles/reunionesRemotas.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/usuario/Desktop/LabAlgo1/tp2/reunionesRemotas/main.cpp" "/Users/usuario/Desktop/LabAlgo1/tp2/reunionesRemotas/cmake-build-debug/CMakeFiles/reunionesRemotas.dir/main.cpp.o"
+  "/Users/usuario/Desktop/LabAlgo1/tp2/reunionesRemotas/src/auxiliares.cpp" "/Users/usuario/Desktop/LabAlgo1/tp2/reunionesRemotas/cmake-build-debug/CMakeFiles/reunionesRemotas.dir/src/auxiliares.cpp.o"
+  "/Users/usuario/Desktop/LabAlgo1/tp2/reunionesRemotas/src/solucion.cpp" "/Users/usuario/Desktop/LabAlgo1/tp2/reunionesRemotas/cmake-build-debug/CMakeFiles/reunionesRemotas.dir/src/solucion.cpp.o"
+  "/Users/usuario/Desktop/LabAlgo1/tp2/reunionesRemotas/tests/acelerar.cpp" "/Users/usuario/Desktop/LabAlgo1/tp2/reunionesRemotas/cmake-build-debug/CMakeFiles/reunionesRemotas.dir/tests/acelerar.cpp.o"
+  "/Users/usuario/Desktop/LabAlgo1/tp2/reunionesRemotas/tests/esReunionValida.cpp" "/Users/usuario/Desktop/LabAlgo1/tp2/reunionesRemotas/cmake-build-debug/CMakeFiles/reunionesRemotas.dir/tests/esReunionValida.cpp.o"
+  "/Users/usuario/Desktop/LabAlgo1/tp2/reunionesRemotas/tests/esSenial.cpp" "/Users/usuario/Desktop/LabAlgo1/tp2/reunionesRemotas/cmake-build-debug/CMakeFiles/reunionesRemotas.dir/tests/esSenial.cpp.o"
+  "/Users/usuario/Desktop/LabAlgo1/tp2/reunionesRemotas/tests/filtradoMediana.cpp" "/Users/usuario/Desktop/LabAlgo1/tp2/reunionesRemotas/cmake-build-debug/CMakeFiles/reunionesRemotas.dir/tests/filtradoMediana.cpp.o"
+  "/Users/usuario/Desktop/LabAlgo1/tp2/reunionesRemotas/tests/hablantesSuperpuestos.cpp" "/Users/usuario/Desktop/LabAlgo1/tp2/reunionesRemotas/cmake-build-debug/CMakeFiles/reunionesRemotas.dir/tests/hablantesSuperpuestos.cpp.o"
+  "/Users/usuario/Desktop/LabAlgo1/tp2/reunionesRemotas/tests/ordenar.cpp" "/Users/usuario/Desktop/LabAlgo1/tp2/reunionesRemotas/cmake-build-debug/CMakeFiles/reunionesRemotas.dir/tests/ordenar.cpp.o"
+  "/Users/usuario/Desktop/LabAlgo1/tp2/reunionesRemotas/tests/ralentizar.cpp" "/Users/usuario/Desktop/LabAlgo1/tp2/reunionesRemotas/cmake-build-debug/CMakeFiles/reunionesRemotas.dir/tests/ralentizar.cpp.o"
+  "/Users/usuario/Desktop/LabAlgo1/tp2/reunionesRemotas/tests/reconstruir.cpp" "/Users/usuario/Desktop/LabAlgo1/tp2/reunionesRemotas/cmake-build-debug/CMakeFiles/reunionesRemotas.dir/tests/reconstruir.cpp.o"
+  "/Users/usuario/Desktop/LabAlgo1/tp2/reunionesRemotas/tests/seEnojo.cpp" "/Users/usuario/Desktop/LabAlgo1/tp2/reunionesRemotas/cmake-build-debug/CMakeFiles/reunionesRemotas.dir/tests/seEnojo.cpp.o"
+  "/Users/usuario/Desktop/LabAlgo1/tp2/reunionesRemotas/tests/silencios.cpp" "/Users/usuario/Desktop/LabAlgo1/tp2/reunionesRemotas/cmake-build-debug/CMakeFiles/reunionesRemotas.dir/tests/silencios.cpp.o"
+  "/Users/usuario/Desktop/LabAlgo1/tp2/reunionesRemotas/tests/tonosDeVozElevados.cpp" "/Users/usuario/Desktop/LabAlgo1/tp2/reunionesRemotas/cmake-build-debug/CMakeFiles/reunionesRemotas.dir/tests/tonosDeVozElevados.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/usuario/Desktop/LabAlgo1/tp2/reunionesRemotas/cmake-build-debug/CMakeFiles/gtest.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
